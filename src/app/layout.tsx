5056edd19a56ac9813ccd27d6a815c37b3d13d15
Chandra Bose <chandrabosep3112@gmail.com>
2024-10-03 02:03:33 +0530

    feat: createEvent

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import QueryProviderWrapper from "@/components/provider/query.provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -29,7 +30,10 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} w-full h-full`}
 			>
-				<QueryProviderWrapper>{children}</QueryProviderWrapper>
+				<QueryProviderWrapper>
+					{children}
+					<Toaster />
+				</QueryProviderWrapper>
 			</body>
 		</html>
 	);
