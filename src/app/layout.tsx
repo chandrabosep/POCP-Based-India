30f6c1e89c75d747099532d3dc3540551f65a3b5
Chandra Bose <chandrabosep3112@gmail.com>
2024-10-06 18:55:06 +0530

    feat: CB wallet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import QueryProviderWrapper from "@/components/provider/query.provider";
 import { Toaster } from "@/components/ui/toaster";
 import Navbar from "@/components/navbar";
+import { Web3Providers } from "@/components/provider/onchain-kit.provider";
+import "@coinbase/onchainkit/styles.css";
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -14,13 +16,18 @@ export default function RootLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
+	
 	return (
 		<html lang="en">
 			<body className={`w-full h-full`}>
 				<QueryProviderWrapper>
-					<Navbar />
-					<div className="w-11/12 h-full mx-auto max-w-screen-2xl">{children}</div>
-					<Toaster />
+					<Web3Providers>
+						<Navbar />
+						<div className="w-11/12 h-full mx-auto max-w-screen-2xl">
+							{children}
+						</div>
+						<Toaster />
+					</Web3Providers>
 				</QueryProviderWrapper>
 			</body>
 		</html>
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,36 @@
+"use client";
+import { ConnectWallet, Wallet } from "@coinbase/onchainkit/wallet";
 import React from "react";
+import { useAccount } from "wagmi";
+import {
+	Avatar,
+	Identity,
+	Name,
+	Badge,
+	Address,
+} from "@coinbase/onchainkit/identity";
 
 export default function Navbar() {
+	const { address } = useAccount();
+	console.log(address);
 	return (
-		<div className="w-full py-4 border-b border-t-slate-100 bg-background">
+		<div className="w-full py-4 border-b border-t-slate-100 bg-[#F9FAFB]">
 			<div className="w-11/12 mx-auto max-w-screen-2xl flex items-center justify-between">
 				<h5 className="text-lg tracking-wider">POCP</h5>
+				{!address ? (
+					<ConnectWallet />
+				) : (
+					<Identity
+						address={address}
+						schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de95317970f0de9"
+					>
+						<Avatar />
+						<Name>
+							<Badge />
+						</Name>
+						<Address />
+					</Identity>
+				)}
 			</div>
 		</div>
 	);
