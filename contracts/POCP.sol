53470a9f406b98689d70e232f45f888d1b98f4e9
mujahid002 <mujahidshaik2002@gmail.com>
2024-10-11 00:45:06 +0530

    updated with onAttest Logic

--- contracts/POCP.sol
+++ contracts/POCP.sol
@@ -9,6 +9,10 @@ error POCP__InvalidInput();
 
 /// @custom:security-contact mujahidshaik2002@gmail.com
 contract POCP is SchemaResolver, Ownable {
+    mapping(address => mapping(string => uint8))
+        private s_userEventAttestations;
+    mapping(address => uint256) private s_reputationScores;
+
     constructor(
         IEAS _eas,
         address _nounContractAddress
@@ -17,13 +21,35 @@ contract POCP is SchemaResolver, Ownable {
     function onAttest(
         Attestation calldata attestation,
         uint256 /*value*/
-    ) internal pure override returns (bool) {
+    ) internal override returns (bool) {
+        (
+            string memory eventId,
+            string memory eventType,
+            uint64 eventTimestamp,
+            uint8 connectionCount,
+            address[] memory connectedAddresses
+        ) = abi.decode(
+                attestation.data,
+                (string, string, uint64, uint8, address[])
+            );
+        if (s_userEventAttestations[attestation.attester][eventId] != 0)
+            revert();
+        if (block.timestamp <= uint256(eventTimestamp) + 24 hours) revert();
+        if (
+            connectedAddresses.length == 0 ||
+            connectedAddresses.length != uint256(connectionCount)
+        ) revert();
+
+        s_userEventAttestations[attestation.attester][
+            eventId
+        ] = connectionCount;
+        s_reputationScores[attestation.attester] += uint256(connectionCount);
         return true;
     }
 
     function onRevoke(
         Attestation calldata /*attestation*/,
-        uint256 /* tNounId*/
+        uint256 /* value*/
     ) internal pure override returns (bool) {
         return false;
     }
