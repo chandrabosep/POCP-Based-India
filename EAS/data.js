0a45bb7e5c8a566663bc3e8eee74de1e2ed52e88
mujahid002 <mujahidshaik2002@gmail.com>
2024-10-16 18:23:07 +0530

    sample data

---
+++ EAS/data.js
@@ -0,0 +1,21 @@
+export const eventData = {
+    "eventId": "Based HYDDAO",
+    "eventType": "VIRTUAL",
+
+    "registrationCount": 10, // Just for INFO
+    "attendeeCount": 6 // Just for INFO
+}
+export const userData = [
+    {
+        "id": 0,
+        "userAddress": "0x1c620232Fe5Ab700Cc65bBb4Ebdf15aFFe96e1B5",
+        "connectionCount": "1",
+        "connectedAddresses": ["0x2CaC7B18c6b67275B1236a332B0218AeD5979A19"]
+    },
+    {
+        "id": 1,
+        "userAddress": "0x271BAbCf5607Ce08ee57081b2dBfb75ACB584052",
+        "connectionCount": "1",
+        "connectedAddresses": ["0x2CaC7B18c6b67275B1236a332B0218AeD5979A19"]
+    }
+]
