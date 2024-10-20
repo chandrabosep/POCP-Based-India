6cd88c338d4814d766614e1b8408f49639142ff2
mujahid002 <mujahidshaik2002@gmail.com>
2024-10-20 21:45:40 +0530

    POCP Hangout Data

--- EAS/data.js
+++ EAS/data.js
@@ -1,21 +1,45 @@
 export const eventData = {
-    "eventId": "Based HYDDAO",
+    "eventId": "POCP Hangout",
     "eventType": "VIRTUAL",
 
-    "registrationCount": 10, // Just for INFO
-    "attendeeCount": 6 // Just for INFO
+    "registrationCount": 7, // Just for INFO
+    "attendeeCount": 7 // Just for INFO
 }
 export const userData = [
     {
         "id": 0,
-        "userAddress": "0x1c620232Fe5Ab700Cc65bBb4Ebdf15aFFe96e1B5",
-        "connectionCount": "1",
-        "connectedAddresses": ["0x2CaC7B18c6b67275B1236a332B0218AeD5979A19"]
+        "userAddress": "0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093",
+        "connectionCount": "5",
+        "connectedAddresses": ["0xD219255bD31C8F1401F016ff2931890c431441B2", "0x222B97c2340Be9bBAf13D61a1d5B312D8D4c9ffb", "0xA6915454683b4F8db50cD46A98eA1db879Eb9EA5", "0xC625e43a102014741aa23207EB5Df87908cbada4", "0x21F695E139719ecF01E1b1D799384587ebAF6cc5"]
     },
     {
         "id": 1,
-        "userAddress": "0x271BAbCf5607Ce08ee57081b2dBfb75ACB584052",
+        "userAddress": "0xD219255bD31C8F1401F016ff2931890c431441B2",
+        "connectionCount": "3",
+        "connectedAddresses": ["0xA6915454683b4F8db50cD46A98eA1db879Eb9EA5", "0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093", "0x21F695E139719ecF01E1b1D799384587ebAF6cc5"]
+    },
+    {
+        "id": 2,
+        "userAddress": "0x222B97c2340Be9bBAf13D61a1d5B312D8D4c9ffb",
         "connectionCount": "1",
-        "connectedAddresses": ["0x2CaC7B18c6b67275B1236a332B0218AeD5979A19"]
-    }
+        "connectedAddresses": ["0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093"]
+    },
+    {
+        "id": 3,
+        "userAddress": "0xA6915454683b4F8db50cD46A98eA1db879Eb9EA5",
+        "connectionCount": "4",
+        "connectedAddresses": ["0xD219255bD31C8F1401F016ff2931890c431441B2", "0x222B97c2340Be9bBAf13D61a1d5B312D8D4c9ffb", "0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093", "0xC625e43a102014741aa23207EB5Df87908cbada4"]
+    },
+    {
+        "id": 4,
+        "userAddress": "0xC625e43a102014741aa23207EB5Df87908cbada4",
+        "connectionCount": "3",
+        "connectedAddresses": ["0xA6915454683b4F8db50cD46A98eA1db879Eb9EA5", "0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093", "0xD219255bD31C8F1401F016ff2931890c431441B2"]
+    },
+    {
+        "id": 5,
+        "userAddress": "0x21F695E139719ecF01E1b1D799384587ebAF6cc5",
+        "connectionCount": "2",
+        "connectedAddresses": ["0xdFbE0Ef6C5421dAdd7AE01d83d5310c9fbc88093", "0xA6915454683b4F8db50cD46A98eA1db879Eb9EA5"]
+    },
 ]
